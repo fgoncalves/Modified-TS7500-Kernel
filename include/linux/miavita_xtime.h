8c5f64752979f8fe9624af814ee4aaeae4502632
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2011-09-05 20:51:08 +0100

    Changed function to .h file

--- include/linux/miavita_xtime.h
+++ include/linux/miavita_xtime.h
@@ -1,6 +1,19 @@
 #ifndef __MIAVITA_XTIME_H__
 #define __MIAVITA_XTIME_H__
 
-extern void pulse_miavita_xtime(void);
-extern void init_miavita_xtime(void);
+#include <linux/time.h>
+
+struct timeval __miavitaxtime = {0, 0};
+
+static inline void init_miavita_xtime(void){
+  do_gettimeofday(&__miavitaxtime);
+  __miavitaxtime.tv_sec = 0;
+}
+
+static inline void pulse_miavita_xtime(void){
+  struct timeval t;
+  do_gettimeofday(&t);
+  __miavitaxtime.tv_usec = t.tv_usec;
+  __miavitaxtime.tv_sec += 1;
+}
 #endif
--- ipc/miavita_syscall.c
+++ ipc/miavita_syscall.c
@@ -1,22 +1,6 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
-#include <linux/time.h>
 #include <linux/miavita_xtime.h>
-
-struct timeval __miavitaxtime = {0, 0};
-
-void init_miavita_xtime(void){
-  do_gettimeofday(&__miavitaxtime);
-  __miavitaxtime.tv_sec = 0;
-}
-
-void pulse_miavita_xtime(void){
-  struct timeval t;
-  do_gettimeofday(&t);
-  __miavitaxtime.tv_usec = t.tv_usec;
-  __miavitaxtime.tv_sec += 1;
-}
-
 /*
  * Mia-Vita's syscall to know microseconds from gps.
  * This must return the seconds with microsecond precision.
